31901e8edcf4a15259342d971212cd509c617691
Ahmed Wael <awael3977@gmail.com>
2023-08-25 16:58:49 +0300

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import About from "@/components/sections/about";
-import Intro from "@/components/sections/intro";
 import LatestProjects from "@/components/sections/latest-projects";
 import Skills from "@/components/sections/skills";
 
 export default function Home() {
 	return (
 		<>
-			<Intro />
+			{/* <Intro /> */}
 			<About />
 			<LatestProjects />
 			<Skills />
